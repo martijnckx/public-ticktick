ccc3c4faebdadb8204f523034ae5f9e089d1ff07
Martijn Luyckx <m@rtijnluyckx.be>
2023-03-29 08:42:46 +0200

    oops v2

--- functions/confirmList.js
+++ functions/confirmList.js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
     if (isValidFormat(searchParams.get('id'))) {
 
         // For now only allow the actual wishlist
-        if (!searchParams.get('id') !== '6234a9aed14ad17eaad2a118') return new Response('not allowed');
+        if (searchParams.get('id') !== '6234a9aed14ad17eaad2a118') return new Response('not allowed');
 
         context.env.WISHLIST.put('martijn-list', searchParams.get('id'));
     }
